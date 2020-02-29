46d029bdc5d2a93642a0e3373c7420aae2f57d8c
Christof Nolle <mail@christof-nolle.de>
2020-02-29 19:52:01 +0100

    tests: parse subscription config

--- src/test/scala/akka/stream/alpakka/nats/NatsStreamingSettingsSpec.scala
+++ src/test/scala/akka/stream/alpakka/nats/NatsStreamingSettingsSpec.scala
@@ -1,7 +1,14 @@
 package akka.stream.alpakka.nats
 
-import java.time.Duration
+import java.time.{Duration, Instant}
 
+import akka.stream.alpakka.nats.DeliveryStartPosition.{
+  AfterSequenceNumber,
+  AfterTime,
+  AllAvailable,
+  LastReceived,
+  OnlyNew
+}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -27,9 +34,20 @@ class NatsStreamingSettingsSpec extends AnyWordSpec with Matchers {
     publishMaxInFlight = publishMaxInFlight,
     discoverPrefix = discoverPrefix
   )
+  val simpleSubSettings = SimpleSubscriptionSettings(
+    subjects = List("sub1", "sub2"),
+    subscriptionQueue = "subQueue",
+    durableSubscriptionName = Some("durableName"),
+    startPosition = AfterSequenceNumber(10),
+    subMaxInFlight = Some(100),
+    bufferSize = 100,
+    autoRequeueTimeout = Some(Duration.ofSeconds(30)),
+    manualAcks = true
+  )
   val connectionConf = ConfigFactory.parseResources("connection.conf")
+  val subscriptionConf = ConfigFactory.parseResources("subscription.conf")
 
-  "NatsStreamingSettings" should {
+  "NatsStreamingConnectionSettings" should {
     "be loaded from config as expected" in {
       NatsStreamingConnectionSettings.fromConfig(
         connectionConf.getConfig("connection-url")
@@ -39,6 +57,42 @@ class NatsStreamingSettingsSpec extends AnyWordSpec with Matchers {
       NatsStreamingConnectionSettings.fromConfig(
         connectionConf.getConfig("connection-host-port")
       ) shouldBe connectionSettings.copy(url = urlHostPort)
+    }
+  }
+
+  "SimpleSubscriptionSettings" should {
+    "be loaded from config as expected with after seq nr. delivery" in {
+      SimpleSubscriptionSettings.fromConfig(
+        subscriptionConf.getConfig("after-seq")
+      ) shouldBe simpleSubSettings
+    }
+    "be loaded from config as expected with after millis" in {
+      SimpleSubscriptionSettings.fromConfig(
+        subscriptionConf.getConfig("deliver-after-epoch")
+      ) shouldBe simpleSubSettings.copy(
+        startPosition = AfterTime(Instant.ofEpochMilli(100))
+      )
+    }
+    "be loaded from config as expected deliver only new" in {
+      SimpleSubscriptionSettings.fromConfig(
+        subscriptionConf.getConfig("deliver-only-new")
+      ) shouldBe simpleSubSettings.copy(
+        startPosition = OnlyNew
+      )
+    }
+    "be loaded from config as expected deliver all available" in {
+      SimpleSubscriptionSettings.fromConfig(
+        subscriptionConf.getConfig("deliver-all-available")
+      ) shouldBe simpleSubSettings.copy(
+        startPosition = AllAvailable
+      )
+    }
+    "be loaded from config as expected deliver last received" in {
+      SimpleSubscriptionSettings.fromConfig(
+        subscriptionConf.getConfig("deliver-last-received")
+      ) shouldBe simpleSubSettings.copy(
+        startPosition = LastReceived
+      )
     }
   }
 
